a02238b6dd3d761015c661f30cc8fa2b7ac8b191
Jaha96 <j_jaha28@yahoo.com>
2016-11-27 12:54:25 +0800

    Add Tessdata

--- TesseractWeb/Controllers/HomeController.cs
+++ TesseractWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +12,20 @@ namespace TesseractWeb.Controllers
         public ActionResult Index()
         {
             ViewBag.Title="My Bootstrap Site";
-return View();
+            return View();
+        }
+
+        public void langList()
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Server.MapPath(@"~/tessdata"));
+            FileInfo[] Files = dinfo.GetFiles("*.traineddata");
+            List<SelectListItem> items = new List<SelectListItem>();
+            foreach (FileInfo file in Files)
+            {
+                string[] val = file.Name.ToString().Split('.');
+                items.Add(new SelectListItem { Text = file.Name, Value = val[0] });
+            }
+            ViewBag.LangList = items.ToArray();
         }
     }
 }
