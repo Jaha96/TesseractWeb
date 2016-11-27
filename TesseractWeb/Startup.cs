a3661b5592e3dcc6f6a386a243a0c4f68df5862b
Jaha96 <j_jaha28@yahoo.com>
2016-11-27 12:46:32 +0800

    Startup

---
+++ TesseractWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TesseractWeb.Startup))]
+namespace TesseractWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
